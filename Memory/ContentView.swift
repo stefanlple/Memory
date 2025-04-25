6f92821732c3620f5fe7582d14851e9dae09b472
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2025-04-25 15:46:03 +0200

    Initial Commit

---
+++ Memory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Stefan Le on 25.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
